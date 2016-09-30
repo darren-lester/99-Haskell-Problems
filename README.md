d7f94755c49c0446912d90c64a04aa629ccf6729
Darren Lester <darrenlester@live.co.uk>
2016-09-30 21:15:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#99 Haskell Problems
+Working through [99 Haskell Problems](https://wiki.haskell.org/H-99:_Ninety-Nine_Haskell_Problems)
+whenever I get a spare moment!
+
+I've tried to solve the problems with basic techniques likes recursion and folds where possible so I do not rely on
+too many Prelude functions.
---
+++ solutions.hs
@@ -0,0 +1,48 @@
+-- Problem 1 Find the last element of a list.
+
+-- using recursion
+last' :: [a] -> a
+last' [] = error "last': empty list"
+last' (x:[]) = x
+last' (x:xs) = last' xs
+
+-- using !! and length
+last'' :: [a] -> a
+last'' [] = error "last'': empty list"
+last'' xs = xs !! (length xs - 1)
+
+-- Problem 2 Find the last but one element of a list.
+secondLast :: [a] -> a
+secondLast [] = error "secondLast: empty list"
+secondLast (x:xs)
+ | length (x:xs) < 2 = error "secondLast: length of list less than 2" 
+ | length (x:xs) == 2 = x
+ | otherwise = secondLast xs
+
+-- Problem 3 Find the K'th element of a list. The first element in the list is number 1.
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "elementAt: empty list"
+elementAt _ 0 = error "elementAt: 0 index"
+elementAt xs i = xs !! (i - 1)
+
+-- Problem 4 Find the number of elements of a list.
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+-- Problem 5 Reverse a list.
+
+-- with left fold
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' xs = foldl (\acc -> \elt -> elt:acc) [] xs
+
+-- with right fold
+reverse'' :: [a] -> [a]
+reverse'' [] = []
+reverse'' xs = foldr (\elt -> \acc -> acc ++ [elt]) [] xs
+
+-- Problem 6 Find out whether a list is a palindrome.
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = False
+isPalindrome xs = xs == reverse' xs
