8b7d2a25155cc059b44d0e67e5542a7274b45561
Darren Lester <darrenlester@live.co.uk>
2016-10-01 12:44:02 +0100

    Add solution to Problem 7

--- solutions.hs
+++ solutions.hs
@@ -46,3 +46,11 @@ reverse'' xs = foldr (\elt -> \acc -> acc ++ [elt]) [] xs
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome [] = False
 isPalindrome xs = xs == reverse' xs
+
+-- Problem 7 Flatten a nested list structure.
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
